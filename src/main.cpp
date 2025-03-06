ec9009ce13804cf60af44ce4b79522f32d8ddcc8
shyravina19102004 <shyravina19102004@gmail.com>
2025-03-06 22:31:26 +0300

    6

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
-// Copyright 2022 UNN-CS
+// Copyright 2023 Your Name
 #include <iostream>
-#include "tasks.h"
 #include "circle.h"
+#include "tasks.h"
+
+double calculateEarthRopeGap() {
+    double earthRadius = 6378100;  // радиус Земли в метрах
+    Circle earth(earthRadius);
+
+    double ropeLength = earth.getFerence() + 1;  // добавляем 1 метр
+    earth.setFerence(ropeLength);
+
+    double gap = earth.getRadius() - earthRadius;  // вычисляем зазор
+    return gap;
+}
+
+double calculatePoolCost() {
+    double poolRadius = 3;  // радиус бассейна в метрах
+    double pathWidth = 1;  // ширина дорожки в метрах
+    double concreteCostPerSquareMeter = 1000;  // стоимость бетона
+    double fenceCostPerMeter = 2000;  // стоимость ограды
+
+    Circle pool(poolRadius);
+    Circle poolWithPath(poolRadius + pathWidth);
+
+    double concreteArea = poolWithPath.getArea() - pool.getArea();
+    double fenceLength = poolWithPath.getFerence();
+
+    double totalConcreteCost = concreteArea * concreteCostPerSquareMeter;
+    double totalFenceCost = fenceLength * fenceCostPerMeter;
+
+    return totalConcreteCost + totalFenceCost;
+}
 
 int main() {
     Circle c(5);
